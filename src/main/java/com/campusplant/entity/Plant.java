8745f72341676235a92dcb65430a9af97e5220f4
Zhang71020 <2957737881@qq.com>
2026-03-06 13:59:08 +0800

    Create Plant.java entity class for plant information management

---
+++ src/main/java/com/campusplant/entity/Plant.java
@@ -0,0 +1,76 @@
+import javax.persistence.*;
+
+@Entity
+@Table(name = "plants")
+public class Plant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String species;
+
+    @Column
+    private String description;
+
+    @Column
+    private Double height;
+
+    @Column
+    private String location;
+
+    // Constructors, Getters and Setters
+    public Plant() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
